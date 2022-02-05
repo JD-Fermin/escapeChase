63522e84d4d8aed208acb9d3f9cfdf002e8b2fa1
Jan Daniel Fermin <jandanielfermin@gmail.com>
2022-02-04 18:14:22 -0800

    Attempt for tilemap

--- src/index.js
+++ src/index.js
@@ -3,19 +3,13 @@ import { Utils } from "./scripts/utils";
 import View from "./scripts/view";
 document.addEventListener("DOMContentLoaded", () => {
   let canvas = document.getElementById("game-box");
+  let canvas2 = document.getElementById("game-box-2");
   let ctx = canvas.getContext("2d");
-  let testPlayer = new MovingObject({pos: [69, 69], radius: 20});
+  let ctx2 = canvas2.getContext('2d');
+  let testPlayer = new MovingObject(ctx);
   let paused = false;
   
-  // let background = new Image();
-  // background.src = "https://image.shutterstock.com/image-vector/arcade-game-world-pixel-scene-600w-1413474734.jpg";
-
-  ctx.fillStyle = "gray";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  
-  testPlayer.draw(ctx);
-  let testView = new View(canvas, ctx, testPlayer);
+  let testView = new View(ctx2, ctx, testPlayer);
   let flag = false;
 
   let reset = document.getElementById('reset');
@@ -42,44 +36,57 @@ document.addEventListener("DOMContentLoaded", () => {
     
   };
   
+  requestAnimationFrame(main);
+  let startTime;
+  let gTime;
+
+  function main(time) {
+    if (startTime === undefined) {
+      startTime = time;
+    }
+    gTime = time - startTime;
+    // ctx.clearRect(0, 0, canvas.width, canvas.height)
+    testView.draw();
+    
+    requestAnimationFrame(main);
+  }
   window.addEventListener("keydown", (event) => {
    
     event.preventDefault();
     if (paused) return;
-    if (event.code === 'KeyE' && testPlayer.collided) {
-      flag = true;
-      Utils.renderMessage(testPlayer.victim);
-      if(!testPlayer.items.includes(testPlayer.victim.item)){
-        testPlayer.items.push(testPlayer.victim.item);
-        if (Utils.levels[1].itemOrder[testPlayer.items.length - 1] !== testPlayer.items[testPlayer.items.length - 1]) {
-          testPlayer.items.pop();
-          let temp = testPlayer.victim.message;
-          testPlayer.victim.message = testPlayer.victim.item === 'success' ? "You need something else to open the door!" : 'You need something else to get this item!';
-          Utils.renderMessage(testPlayer.victim);
-          testPlayer.victim.message = temp;
-        } else {
-          testPlayer.victim.message = "There's nothing here....";
-        }
+    // if (event.code === 'KeyE' && testPlayer.collided) {
+    //   flag = true;
+    //   Utils.renderMessage(testPlayer.victim);
+      // if(!testPlayer.items.includes(testPlayer.victim.item)){
+      //   testPlayer.items.push(testPlayer.victim.item);
+      //   if (Utils.levels[1].itemOrder[testPlayer.items.length - 1] !== testPlayer.items[testPlayer.items.length - 1]) {
+      //     testPlayer.items.pop();
+      //     let temp = testPlayer.victim.message;
+      //     testPlayer.victim.message = testPlayer.victim.item === 'success' ? "You need something else to open the door!" : 'You need something else to get this item!';
+      //     Utils.renderMessage(testPlayer.victim);
+      //     testPlayer.victim.message = temp;
+      //   } else {
+      //     testPlayer.victim.message = "There's nothing here....";
+      //   }
           
         
         
-      } 
-      console.log(testPlayer.items);
-      if(testPlayer.items[testPlayer.items.length - 1] === 'success'){
-        Utils.renderMessage(testPlayer.victim);
-        alert('You escaped!');
-        location.reload();
-      }
+      // } 
+      // console.log(testPlayer.items);
+      // if(testPlayer.items[testPlayer.items.length - 1] === 'success'){
+      //   Utils.renderMessage(testPlayer.victim);
+      //   alert('You escaped!');
+      //   location.reload();
+      // }
 
-      setTimeout(() => {
-        flag = false
-      }, 4000);
-    }
+      // setTimeout(() => {
+      //   flag = false
+      // }, 4000);
+    // }
     
     if (flag === false){
-      testPlayer.move(testPlayer.convertKeytoDir(event.code), testView, true);
-      console.log(testPlayer.collided, testPlayer.victim);
-      testView.draw();
+      testPlayer.move(testPlayer.convertKeytoDir(event.code));
+      requestAnimationFrame(() => testView.draw())
     }
     
     
--- src/scripts/moving_object.js
+++ src/scripts/moving_object.js
@@ -1,27 +1,39 @@
 import { Utils } from "./utils.js";
 class MovingObject {
-  constructor(attributes) {
-    this.pos = attributes['pos'];
-    this.radius = attributes['radius'];
-    this.collided = false;
-    this.victim = null;
-    this.items = [];
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.pos = [240, 160];
+    this.mapPos = [10, 15];
+    this.canvas = document.getElementById('game-box')
   }
 
-  draw(ctx) {
-   
-    let circle = new Path2D();
-    
-    ctx.beginPath();
-    circle.arc(this.pos[0], this.pos[1], this.radius, 0, Math.PI * 2, false);
-    ctx.fillStyle = 'green';
-    ctx.fill(circle);
-    // let image = new Image();
-    // image.onload = () => {
-    //   ctx.drawImage(image, this.pos[0], this.pos[1], this.radius, this.radius)
-    // }
-    // image.src = 'src/knight_f_idle_anim_f1.png'
-    
+  draw() {
+    // this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    let char = new Image();
+    char.src = "./assets/spritesheet.png";
+    char.onload = () => {
+      const scale = 1;
+      const width = 16;
+      const height = 16;
+      const scaledWidth = scale * width;
+      const scaledHeight = scale * height;
+
+      let drawFrame = (frameX) => {
+        this.ctx.drawImage(
+          char,
+          frameX * width,
+          0,
+          width,
+          height,
+          this.pos[0],
+          this.pos[1],
+          scaledWidth,
+          scaledHeight
+        );
+      };
+
+      drawFrame(0);
+    };
   }
 
   convertKeytoDir(code) {
@@ -29,11 +41,11 @@ class MovingObject {
       case "KeyS":
       case "ArrowDown":
         return [0, 1];
-     
+
       case "KeyW":
       case "ArrowUp":
         return [0, -1];
-      
+
       case "KeyA":
       case "ArrowLeft":
         return [-1, 0];
@@ -41,34 +53,21 @@ class MovingObject {
       case "KeyD":
       case "ArrowRight":
         return [1, 0];
-      
     }
   }
 
-  move(dir, view) {
-    let dx = this.radius/2 * dir[0];
-    let dy = this.radius/2 * dir[1];
+  move(dir) {
+    let dx = 16 * dir[0];
+    let dy = 16 * dir[1];
     
-    if(this.pos[0] + dx < view.canvas.width - this.radius && this.pos[0] + dx > this.radius && this.pos[1] + dy < view.canvas.height - this.radius && this.pos[1] + dy > this.radius ) {
-
       this.pos[0] += dx;
       this.pos[1] += dy;
-      for (let i = 0; i < view.objs.length; i++) {
-        if (Utils.detectCollision(this, view.objs[i])){
-          this.collided = true;
-          this.victim = view.objs[i];
-          this.pos[0] -= dx;
-          this.pos[1] -= dy;
-          
-          break;
-        } else {
-          this.collided = false;
-          this.victim = null;
-        }
 
-      }
-    }
-   
+      this.mapPos[0] += dir[1];
+      this.mapPos[1] += dir[0];
+
+      
+    
   }
 }
 
